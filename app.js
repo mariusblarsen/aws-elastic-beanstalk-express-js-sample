0838138681795cbcfc773e7dd9fe53adbaf5c902
Marius Larsen <marbl@Mariuss-MacBook-Pro.local>
2022-08-24 13:43:58 +0200

    hello bar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Foo!'));
+app.get('/', (req, res) => res.send('Hello Bar!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
